8a10bd2b0845baf8dc0733c453e1b5403dafba4a
Alex Epik <73494181+EvgeniyPukanovich@users.noreply.github.com>
2023-04-02 23:42:18 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,17 @@ from scipy.stats import norm
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    # оцениваем параметры распределения ошибок измерения
+    mu = np.mean(x)
+    s = np.std(x, ddof=1)
+    # вычисляем стандартную ошибку среднего
+    se = s / np.sqrt(n)
+    # вычисляем критическое значение
+    z = norm.ppf(1 - alpha/2)
+    # вычисляем интервал
+    interval = z * se
+    lower = mu - interval
+    upper = mu + interval
+    return (lower, upper)
